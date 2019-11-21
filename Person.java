ce7684681b40dd3ce02678cdfd93160328b847ac
Martin Yanchev <martinm199913@gmail.com>
2019-11-21 13:38:11 +0000

    Person class added getters and setters

--- Person.java
+++ Person.java
@@ -1,9 +1,23 @@
 public class Person{
-    String fistName;
-    String other;
-    String surname;
-    public Person(String param1,String param2){
-
+   private String fistName;
+    
+   private String surname;
+    public Person(String firstName,String surname){
+        this.fistName = firstName;
+        this.surname = surname;
     }
+    public String getFisrtName(){
+        return firstName;
+    }
+    public String getSurname(){
+        return surname;
+    }
+    public String setFistName(String newFisrtName){
+        return this.firstName = newFisrtName;
+    }
+    public String setSurname(String newSurname){
+        return this.firstName = newSurname;
+    }
+
 
 }
